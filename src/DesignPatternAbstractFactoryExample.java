f5715430a29edb7901396679a2554e7843daf6d7
pwgtiluser.email <87475830+pwgtil@users.noreply.github.com>
2023-07-10 17:52:07 +0200

    feat: implement Abstract Factory design pattern

---
+++ src/DesignPatternAbstractFactoryExample.java
@@ -0,0 +1,25 @@
+import app.Application;
+import factories.GUIFactory;
+import factories.MacOSFactory;
+import factories.WindowsFactory;
+
+public class DesignPatternAbstractFactoryExample {
+
+    private static Application configureApplication() {
+        Application app;
+        GUIFactory factory;
+        String osName = System.getProperty("os.name").toLowerCase();
+        if (osName.contains("mac")) {
+            factory = new MacOSFactory();
+        } else {
+            factory = new WindowsFactory();
+        }
+        app = new Application(factory);
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Application app = configureApplication();
+        app.paint();
+    }
+}
---
+++ src/factories/GUIFactory.java
@@ -0,0 +1,12 @@
+package factories;
+
+import buttons.Button;
+import checkboxes.Checkbox;
+
+/**
+ * Abstract factory knows about all (abstract) product types.
+ */
+public interface GUIFactory {
+    Button createButton();
+    Checkbox createCheckbox();
+}
